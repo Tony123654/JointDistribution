0a9f66f00b5b936b635eeeb4920d77b34aa180ed
yanjiang <bluesatan0-0@gmail.com>
2017-08-19 14:07:36 +0800

    1.完成：初始界面，根据账户记录默认登陆 2.完成：登录界面，登陆后按角色分发页面，存储账户记录 3.完成：页面切换后内存泄漏的修复：原生volley在页面onDestory时还持有引用(listener)

--- app/src/main/java/com/acuit/jointdistribution/Common/Activity/LoginActivity.java
+++ app/src/main/java/com/acuit/jointdistribution/Common/Activity/LoginActivity.java
@@ -8,7 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.acuit.jointdistribution.Common.Presenter.LoginPresenter;
 import com.acuit.jointdistribution.R;
+import com.acuit.jointdistribution.Storeman.Activity.MainActivity;
 import com.acuit.jointdistribution.Supplier.Acitivity.HomeActivity;
 
 import static com.acuit.jointdistribution.R.id.et_mm;
@@ -19,26 +21,35 @@ import static com.acuit.jointdistribution.R.id.et_zh;
  * <p>
  * <p>
  * 修改人：yanj
- * 修改时间：2017-08-17 11:49:57
+ * 修改时间：2017-08-19 10:49
+ * 修改内容：实现登录
  */
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements LoginView_Interface, View.OnClickListener {
 
     private EditText etAccount;
     private EditText etPsw;
     private Button btnLogin;
-    private String account;
-    private String pwd;
+    private String account = null;
+    private String phone = null;
+    private String pwd = null;
+    private LoginPresenter loginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        initView();
+
+        loginPresenter = new LoginPresenter(this);
+
+        btnLogin.setOnClickListener(this);
+    }
+
+    private void initView() {
         etAccount = (EditText) findViewById(et_zh);
         etPsw = (EditText) findViewById(et_mm);
         btnLogin = (Button) findViewById(R.id.btn);
-
-        btnLogin.setOnClickListener(this);
     }
 
 
@@ -50,19 +61,47 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         if (account.isEmpty()) {
             Toast.makeText(this, "账户不能为空", Toast.LENGTH_SHORT).show();
+            return;
         }
         if (pwd.isEmpty()) {
             Toast.makeText(this, "密码不能为空", Toast.LENGTH_SHORT).show();
+            return;
         }
 
 
 
+        {
+//        合法性校验（11位纯数字则赋值给phone）
+
+        }
+
+        loginPresenter.login(account, phone, pwd);
 
 
-        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+    }
 
 
+    @Override
+    public void startLoginActivity() {
+    }
 
-        startActivity(intent);
+    @Override
+    public void startStoremanHome() {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
+    @Override
+    public void startSupplierHome() {
+        startActivity(new Intent(this, HomeActivity.class));
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+    }
+
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 }
